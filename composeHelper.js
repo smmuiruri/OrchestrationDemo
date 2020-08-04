7f656b1951e9d6053c0a0977dfa4661b5fa9b2c8
vinge1718 <vinge1718@gmail.com>
2020-08-04 22:26:45 +0300

    Added the directives to edit the node js file in the orchestration demo

--- composeHelper.js
+++ composeHelper.js
@@ -5,18 +5,12 @@ var http = require('http');
 var url = require('url');
 var redis = require('redis');
 
-// Connect to redis server running
-// createClient API is called with
-//  -- 6379, a well-known port to which the
-//           redis server listens to
-//  -- redis, is the name of the service (container)
-//            that runs redis server
+// Connect to redis server running createClient API is called with-- 6379, a well-known port to which the
+// redis server listens to -- redis, is the name of the service (container) that runs redis server
 var client = redis.createClient(6379, 'redis');
 
-// Set the key value pair in the redis server
-
-// Here all the keys proceeds with "/", because
-// URL parser always have "/" as its first character
+// Set the key value pair that we are going to be querying in the redis server. Here all the keys proceeds with "/", because
+// URL parser always have "/" as its first character. Again, feel free to add more of these pairs.
 client.set("/", "Welcome to Docker-Compose helper\nEnter the docker-compose command in the URL for help\n", redis.print);
 client.set("/run", "This runs a one-off command", redis.print);
 client.set("/scale", "This sets a number of containers for a service", redis.print);
